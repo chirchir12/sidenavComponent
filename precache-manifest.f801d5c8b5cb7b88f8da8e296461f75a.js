fe5bfd19f3810754c839fb67d0aedbc9e2c6071c
chirchir <chirchir7370@gmail.com>
2020-03-16 16:09:55 +0300

    Updates

---
+++ precache-manifest.f801d5c8b5cb7b88f8da8e296461f75a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98248bb1c035701cda723779e2f38aca",
+    "url": "/chirchir12/sidenavComponent/index.html"
+  },
+  {
+    "revision": "f066290111e0aa5f9232",
+    "url": "/chirchir12/sidenavComponent/static/css/main.dcd44cd3.chunk.css"
+  },
+  {
+    "revision": "d557fb2849bcc40d802c",
+    "url": "/chirchir12/sidenavComponent/static/js/2.44beccfb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chirchir12/sidenavComponent/static/js/2.44beccfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f066290111e0aa5f9232",
+    "url": "/chirchir12/sidenavComponent/static/js/main.969c23f6.chunk.js"
+  },
+  {
+    "revision": "f792bf62ac9ed5d521bd",
+    "url": "/chirchir12/sidenavComponent/static/js/runtime-main.0bcbcdc9.js"
+  }
+]);
